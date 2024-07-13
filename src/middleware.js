581dfc0886cf3c7ac9e838adb52a41421521ec82
MILON-2377 <153436745+MILON-2377@users.noreply.github.com>
2024-07-13 21:08:45 +0800

    middleware added

---
+++ src/middleware.js
@@ -0,0 +1,25 @@
+
+
+import { NextResponse } from 'next/server'
+ 
+// This function can be marked `async` if using `await` inside
+export function middleware(req) {
+  
+    console.log("middleware is working");
+
+        // const token = req.cookies.get("token")?.value || null;
+
+        const token = null;
+    console.log(token);
+
+    if(!token){
+        return NextResponse.redirect(new URL("/login", req.url));
+    }
+
+    return NextResponse.next();
+}
+ 
+// See "Matching Paths" below to learn more
+export const config = {
+  matcher: ['/dashboard'],
+}
